6ab9a69d848cbaf56f375e985575cdf17bb114ab
Tlotliso Mofokeng <tlotliso.mofokeng@yahoo.com>
2021-05-09 11:01:20 +0200

    settings update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,12 +119,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_URL = '/media/'
+
+
+#media url to render the media files
+MEDIA_URL = '/images/'
+
+#Media_root to set the path for all media files that will be uploaded
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/pdf_file')
